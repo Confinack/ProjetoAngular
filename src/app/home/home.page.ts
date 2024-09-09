fc79741fdef28bb7dc2cff4129c093751de93661
Confinack <102000232+Confinack@users.noreply.github.com>
2024-09-09 11:54:05 +0000

    Generos musicais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
+
+  public nome:string = "";
+
+  public lista:any[] = [
+    {
+      Titulo: "Rock",
+    },
+    {
+      Titulo: "Lo-fi"
+    },
+    {
+      Titulo: "Shitrock",
+    },
+    {
+      Titulo: "Romance",
+    },
+    {
+      Titulo: "Nightcore",
+    }
+  ]
+
   constructor() {}
+
+  adicionar(){
+    this.lista.push({Titulo: this.nome});
+    this.nome = "";
+  }
 }
