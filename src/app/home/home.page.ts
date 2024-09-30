0e22a0a6de903f65688c3986be04ccd9d612dc74
Confinack <102000232+Confinack@users.noreply.github.com>
2024-09-30 03:01:50 +0000

    Exercicio de teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
   constructor() {}
+
+  public comida:string = "";
+  public comidas:any[] = [];
+
+  public adicionar(){
+    this.comidas.push({Nome: this.comida});
+    this.comida = "";
+  }
+
+  public remover(objeto:any){
+    const index = this.comidas.indexOf(objeto);
+    this.comidas.splice(index, 1);
+  }
+
 }
